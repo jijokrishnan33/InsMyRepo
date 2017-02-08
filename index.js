f6f79c7d2aa9399f5994f529d5a9db206999b8b4
INSBOT <jijokrishnan33@gmail.com>
2017-02-08 19:14:10 +0530

    fourth

--- index.js
+++ index.js
@@ -10,14 +10,8 @@ bot.use(new BottrApp())
 
 
 bot.on('message_received', function(message, session) {
-
-var button = {
-              "type": "survey",
-              "question": "What would you like to do?",
-              "options": ["Eat", "Drink", "Both"],
-              "msgid": "gt_212"
-             }  
-  session.send(JSON.stringify(button))
+  
+  session.send("Hello may I help You..??")
 })
 
 bot.use(new Bottr.FacebookMessengerClient())
