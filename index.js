c9ddde781d9f7f1db6fbd2c7c3149bf5aa7e9660
INSBOT <jijokrishnan33@gmail.com>
2017-02-09 20:55:44 +0530

    msg

--- index.js
+++ index.js
@@ -9,12 +9,48 @@ bot.use(new BottrApp())
 
 
 
+var attachment = {
+    type: "template",
+    payload: {
+      template_type: "generic",
+      elements: [{
+        title: "rift",
+        subtitle: "Next-generation virtual reality",
+        item_url: "https://www.oculus.com/en-us/rift/",               
+        image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+        buttons: [{
+          type: "web_url",
+          url: "https://www.oculus.com/en-us/rift/",
+          title: "Open Web URL"
+        }, {
+          type: "postback",
+          title: "Call Postback",
+          payload: "Payload for first bubble",
+        }],
+      }, {
+        title: "touch",
+        subtitle: "Your Hands, Now in VR",
+        item_url: "https://www.oculus.com/en-us/touch/",               
+        image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+        buttons: [{
+          type: "web_url",
+          url: "https://www.oculus.com/en-us/touch/",
+          title: "Open Web URL"
+        }, {
+          type: "postback",
+          title: "Call Postback",
+          payload: "Payload for second bubble",
+        }]
+      }]
+    }
+  }
+
 bot.on('message_received', function(message, session) {
   
-  session.send("Hello May I help you ??")
+  session.send("Hello May I help you ??",attachment)
+
 })
 
 bot.use(new Bottr.FacebookMessengerClient())
 
 bot.listen()
-
