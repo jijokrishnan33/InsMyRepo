5c9319177a31fe92cef0de523fa4115c2af4e8ef
INSBOT <jijokrishnan33@gmail.com>
2017-02-22 12:21:11 +0530

    AI files added

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
 var Bottr = require('bottr')
 var fs = require('fs')
 var BottrApp = require('bottr-app')
+var ElizaBot = require('./elizabot');
+var eliza = new ElizaBot();
 
 var bot = new Bottr.Bot()
 bot.use(new BottrApp())
 bot.use(new Bottr.FacebookMessengerClient())
 
-var options = [
-  {
-    index: "1. ",
-	destination: "Get Insured"
-  },
-  {
-    index: "2. ",
-    destination: "Change Insured"
-  },
-  {
-    index: "3. ",
-    destination: "Contact a Representative"
-  }
-]
+bot.on('message_received', function(message, session) {
+  	var reply = eliza.transform(String(message.text));
+ 	session.send(reply);
+})
 
-bot.hears(/hai|hello|hey|hi/i, function(message, session) {
-
-	session.send("May I help you with any of the below services \n")
-	var rows = options.map(function(item){
- 	session.send(item.index +  " " + item.destination.link("http://www.w3schools.com/"));
-	})
-	
-     	
-});
-
-
-function callRestService (session) {
-session.send("Oops! I didn't understood that.I am in development phase only")
-}
 
 bot.listen()
